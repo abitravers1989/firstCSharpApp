997bd964cc9be83a7219f7d8d4ed628eeed12b2c
AbiTravers <abigail.travers@bglgroup.com>
2018-02-01 09:29:54 +0000

    Animal class and main programme

---
+++ YagoSession1Practice/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YagoSession1Practice
+{
+    public class Animal
+    {
+        public string AnimalType { get; private set; }
+        public string AnimalName { get; private set; }
+
+
+        //name of constructor has to be name of class
+        public Animal(string type, string name)
+        {
+            this.AnimalType = type;
+            this.AnimalName = name;
+        }
+
+        public Animal(string type) : this(type, "John")
+        { }
+
+        private static void Opener()
+        {
+            Console.WriteLine("Calling the private opener");
+        }
+
+        public void DescribeMe()
+        {
+            Opener();
+            Console.WriteLine("I am  " + AnimalType + " I am a " + AnimalName);
+        }
+
+    }
+}
---
+++ YagoSession1Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YagoSession1Practice
+{
+    class Program
+    {
+      public static void Main (string [] args)
+        {
+            Animal first = new Animal("cat", "poopie");
+            var second = new Animal("dog");
+
+            first.DescribeMe();
+            second.DescribeMe();
+            Console.ReadLine();
+        }
+    }
+}
