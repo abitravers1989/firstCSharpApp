d0184b0e69b5b904cf66d3d67a1427d606d3ad61
AbiTravers <abigail.travers@bglgroup.com>
2018-02-01 14:19:29 +0000

    Adding a question class. This can be initalised in the program.cs where it can be passed a question to ask

---
+++ YagoSession1Practice/Questionaire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YagoSession1Practice
+{
+    class Questionaire
+    {
+       // public string Answer { get; private set; }
+
+        public string Question { get; private set; }
+
+        public Questionaire(string question)
+        {
+            this.Question = question;
+        }
+
+        public void AskQuestion()
+        {
+            Console.WriteLine("Your Question is:" + question)
+        }
+    }
+}
+
+
